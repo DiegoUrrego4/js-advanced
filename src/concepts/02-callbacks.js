a537b80c549f197eb29123ae7550db3e1dbe8bdc
Diego Urrego <diegourrg@gmail.com>
2023-10-24 14:47:00 -0500

    callbacks understood.

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -5,15 +5,35 @@ import { heroes } from '../data/heroes';
  * @param {HTMLDivElement} element
  */
 export const callbackComponent = element => {
-  console.log('callbackComponent');
+  const id1 = '5d86371f9f80b591f499df32';
+  const id2 = '5d86371fd55e2e2a30fe1ccb2';
+
+  findHero(id1, (error, hero1) => {
+    // element.innerHTML = hero?.name || 'No hero';
+    if (error) {
+      element.innerHTML = error;
+      return;
+    }
+    findHero(id2, (error, hero2) => {
+      if (error) {
+        element.innerHTML = error;
+        return;
+      }
+      element.innerHTML = `${hero1.name} / ${hero2.name}`;
+    });
+  });
 };
 
 /**
  *
  * @param {String} id
- * @param {Function} callback
+ * @param {(error: String|Null, hero: Object)=> void} callback
  */
 const findHero = (id, callback) => {
   const hero = heroes.find(hero => hero.id === id);
-  callback(hero);
+  if (!hero) {
+    callback(`Hero with id ${id} not found`);
+    return; // undefined
+  }
+  callback(null, hero);
 };
